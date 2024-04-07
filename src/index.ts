1a947542cc3373e8cb138a97713094b99bfc3d83
Patrick Villanueva <pkrvillanueva@gmail.com>
2024-04-08 01:34:21 +0800

    Update libraries and use TypeScript

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import './index.css';
+
+// eslint-disable-next-line no-console
+console.log( 'hello world' );
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,9 @@ const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
 module.exports = {
 	...defaultConfig,
+	entry: {
+		script: resolve( process.cwd(), 'src/index.ts' ),
+	},
 	output: {
 		path: resolve( process.cwd(), 'build' ),
 	},
